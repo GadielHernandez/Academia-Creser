7ff73e9e7caeed59ee0219033e06d814fa0848d3
GadielHernandez <gadhdz96@gmail.com>
2021-01-05 18:22:14 -0600

    build: proteger rutas

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 
+import { auth } from '../plugins/firebase'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -10,6 +12,9 @@ const routes = [
         path: '/',
         name: 'Home',
         component: Home,
+        meta: {
+            requiresAuth: true,
+        },
     },
     {
         path: '/login',
@@ -22,6 +27,17 @@ const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes,
+})
+
+router.beforeEach((to, from, next) =>{
+	const requiresAuth = to.matched.some( r => r.meta.requiresAuth )
+	// const requiresAdmin = to.matched.some( r => r.meta.requiresAdmin )
+    
+    if(requiresAuth && !auth.currentUser) return next('/login')
+    else if(!requiresAuth &&auth.currentUser) return next('/')
+    
+    next()
+        
 })
 
 export default router
